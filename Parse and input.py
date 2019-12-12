10e3ffd4496b960515d1717e80e45fa955651821
Pritam <pritam@pop-os.localdomain>
2019-12-12 21:31:09 +0530

    removed venv

--- Parse and input.py
+++ Parse and input.py
@@ -5,3 +5,4 @@ for i in df.index:
     #print(df.loc[i,'title'])
     a=anime(title=df.loc[i,'title'],rank=df.loc[i,'rank'],score=df.loc[i,'score'],users_rated=df.loc[i,'users_rated'],Popularity=df.loc[i,'Popularity'],Favorites=df.loc[i,'Favorites'],Members=df.loc[i,'Members'],animeType=df.loc[i,'animeType'],Episodes=df.loc[i,'Episodes'],status=df.loc[i,'status'],premiered=df.loc[i,'premiered'],studio=df.loc[i,'studio'],source=df.loc[i,'source'],rating=df.loc[i,'rating'],genres=df.loc[i,'genres'])
     a.save()
+print('ho')
