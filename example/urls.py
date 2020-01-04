d75207052060ff192e101e10c87becbe2826e167
Arka45 <arka45@pop-os.localdomain>
2020-01-05 01:01:31 +0530

    Added anyfiveyear and last five year spring anime

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ListAnimeView,GetbyRank,GetTopten
+from .views import ListAnimeView,GetbyRank,GetTopten,GetSpringanime,Lastfive
 from django.conf.urls import url
 urlpatterns=[
     url(r'^$',ListAnimeView.as_view(),name='anime-all'),
     url(r'^rank/(?P<rank>[0-9]+)/$', GetbyRank.as_view(), name='GetByRank'),
-    url(r'^pop10/$',GetTopten.as_view(),name='GetTopten')
+    url(r'^pop10/$',GetTopten.as_view(),name='GetTopten'),
+    url(r'^sprank/(?P<year>[0-9]+)/$',GetSpringanime.as_view(),name='GetSpringanime'),
+    url(r'^last5/$',Lastfive.as_view(),name='Last5'),
     
 ]
--- example/views.py
+++ example/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from.serializers import  AnimeSerializer
 from .models import anime
 from django.http import HttpResponse
+from datetime import datetime
 
 class ListAnimeView(generics.ListAPIView):
     queryset= anime.objects.all()
@@ -19,5 +20,42 @@ class GetTopten(generics.ListAPIView):
     def get_queryset(self):
         sorted=anime.objects.order_by('Popularity')
         return sorted[:10]
+
+class GetSpringanime(generics.ListAPIView):
+    serializer_class=AnimeSerializer
+    def get_queryset(self):
+        year=int(self.kwargs['year'])
+        i=0
+        spring3=anime.objects.filter(premiered='Spring'+' '+str(year))
+        while i<5:
+            year=year-1
+            spring2=anime.objects.filter(premiered='Spring'+' '+str(year))
+            spring3=spring3.union(spring2)
+            i=i+1
+        final=spring3.order_by('rank')
+        return final
+
+
+class Lastfive(generics.ListAPIView):
+    serializer_class=AnimeSerializer
+    def get_queryset(self):
+        year=int(datetime.now().year)
+        i=0
+        spring3=anime.objects.filter(premiered='Spring'+' '+str(year))
+        while i<5:
+            year=year-1
+            spring2=anime.objects.filter(premiered='Spring'+' '+str(year))
+            spring3=spring3.union(spring2)
+            i=i+1
+        final=spring3.order_by('rank')
+        return final
+
+
+
+
+
+
+
+        
 
 
